36263c9b2949115c7cec19d44005482b26d5251c
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-21 09:23:57 -0400

    Adjusted view model to pull to-do items from the database hosted at Supabase.

--- TodoList/Helpers/Supabase.swift
+++ TodoList/Helpers/Supabase.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://agplsldhnwwtwozkwjxg.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFncGxzbGRobnd3dHdvemt3anhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxOTYzMzAsImV4cCI6MjAyODc3MjMzMH0.SB37ESpfJyxs53Qu7wixi655nd3n3GZYfK9idpJxIzs"
+)
+
