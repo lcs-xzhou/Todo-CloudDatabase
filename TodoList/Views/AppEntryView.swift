d12f1aa89031ffb5161b4fd3e05a7450f1864a48
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-23 20:43:26 -0400

    Added an authentication flow so that users are signed-in anonymously and assigned a unique user id. This prevents one user from seeing another user's data.

---
+++ TodoList/Views/AppEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  AppEntryView.swift
+//  TodoList
+//
+//  Created by Fiona ZHOU on 2024-05-23.
+//
+
+import SwiftUI
+
+struct AppEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppEntryView()
+}
---
+++ TodoList/Views/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  TodoList
+//
+//  Created by Fiona ZHOU on 2024-05-23.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
